15ca0706a0d6b1d2b57967d5f51c5448b4e52cc9
Eric7398 <ericwang7398@gmail.com>
2021-01-03 17:03:28 -0800

    update

---
+++ JS_OOP/Ninja.js
@@ -0,0 +1,24 @@
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName() {
+        console.log(`Ninja's name is ${this.name}`);
+    }
+
+    showStats() {
+        console.log(`${this.name} has ${this.health} health, ${this.speed} speed, ${this.strength} strength`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+    }
+
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
