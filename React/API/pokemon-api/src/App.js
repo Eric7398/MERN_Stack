da3436d8c012e9cb921cc5bec6052302d71e0e5a
Eric7398 <ericwang7398@gmail.com>
2021-01-13 07:07:17 -0800

    update

---
+++ React/API/pokemon-api/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PokeDex from './components/PokeDex'
+
+function App() {
+  return (
+    <div className="App">
+      <PokeDex />
+    </div>
+  );
+}
+
+export default App;
